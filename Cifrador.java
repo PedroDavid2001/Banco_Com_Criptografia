fbf6fe2cd4cb387fc101f63ab751f8c7d58c1cdc
PedroDavid2001 <pedrodavirochasaldanha@gmail.com>
2024-03-03 17:11:27 -0300

    att cifrador e criacao de servidor

--- Cifrador.java
+++ Cifrador.java
@@ -1,26 +1,91 @@
 /* 
- Classe responsavel por realizar a cifragem e decifragem de 
- mensagens, bem como a codificacao e decodificao em Base64.
+ Classe responsavel por realizar: 
+ > A cifragem e decifragem de mensagens;
+ > A codificacao e decodificao em Base64;
+ > A geracao de chaves.
 */
 
 import java.util.Base64;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Random;
+
 public class Cifrador {
+
+    private Servidor servidor = null;
     
-    private static String codificar(String texto) 
+    public Cifrador(Servidor servidor)
     {
-        byte[] bytesTXT = texto.getBytes();
-        return Base64.getEncoder().encodeToString(bytesTXT); 
+        if(servidor != null) {
+            this.servidor = servidor;
+        }    
     }
 
-    private static String decodificar(String textoB64) 
-    {
-        String texto = new String(Base64.getDecoder().decode(textoB64));
-        return texto; 
+    public Cifrador()
+    {    
     }
 
-    private static String cifra_vernam(String texto, String chave)
+    /* ======================================= */
+    /*           GERACAO DE CHAVES             */
+    /* ======================================= */ 
+    
+    private static String DIGITOS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+    public static SecretKey gerarChaveAES() throws NoSuchAlgorithmException
     {
+        KeyGenerator gerador = KeyGenerator.getInstance("AES");
+        gerador.init(192);
+        return gerador.generateKey();
+    }
+
+    public static String gerarChaveVernam()
+    {
+        StringBuilder chave = new StringBuilder();
+        for(int i = 0; i < 10; i++){
+            int index = new Random().nextInt(DIGITOS.length());
+            chave.append(DIGITOS.charAt(index));
+        }
+        return chave.toString();
+    }
+    
+    /* ======================================= */
+    /*        CODIFICAO E DECODIFICAO          */
+    /* ======================================= */ 
+
+    private String codificar(byte[] bytes_cifrados) 
+    {
+        return Base64.getEncoder().encodeToString(bytes_cifrados); 
+    }
+
+    private byte[] decodificar(String texto_Base64) 
+    {
+        return Base64.getDecoder().decode(texto_Base64);
+    }
+
+    /* ======================================= */
+    /*               CIFRAGEM                  */
+    /* ======================================= */ 
+
+    public String cifrar_mensagem(String mensagem, String cpf)
+    {
+        String msg_vernam = cifra_vernam(mensagem, cpf);
+        byte [] bytes_cifrados = cifra_AES(msg_vernam, cpf);
+        return codificar(bytes_cifrados);
+    }
+
+    private String cifra_vernam(String texto, String cpf)
+    {
+        String chave = servidor.getChaveVernam(cpf);
         StringBuilder cifrado = new StringBuilder();
         for(int i = 0; i < texto.length(); i++){
             char r = (char) (texto.charAt(i) ^ (i % chave.length()));
@@ -29,8 +94,96 @@ public class Cifrador {
         return cifrado.toString();
     }
 
-    private static String decifragem_vernam(String cifrado, String chave)
+    private byte[] cifra_AES(String texto, String cpf)
     {
-        return cifra_vernam(cifrado, chave);
+        try {
+            // Resgata a chave e atualiza o vetor de 
+            // inicializacao relativo ao cpf do cliente
+            SecretKey chave = servidor.getChaveAES(cpf);
+            servidor.setVetorInit(cpf, gerar_vetor_inicializacao());
+            
+            // Configura o cifrador
+            Cipher cifrador = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            cifrador.init(Cipher.ENCRYPT_MODE, chave, servidor.getVetorInit(cpf));
+
+            // Retorna o array de bytes cifrado
+            byte [] bytes_cifrados = cifrador.doFinal(texto.getBytes());
+            return bytes_cifrados;
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (NoSuchPaddingException e) {
+            e.printStackTrace();
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        } catch (InvalidAlgorithmParameterException e) {
+            e.printStackTrace();
+        } catch (IllegalBlockSizeException e) {
+            e.printStackTrace();
+        } catch (BadPaddingException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    /* ======================================= */
+    /*              DECIFRAGEM                 */
+    /* ======================================= */ 
+
+    public String decifrar_mensagem(String codificado, String cpf)
+    {
+        byte [] bytes_cifrados = decodificar(codificado);
+        String msg_codificada = decifragem_AES(bytes_cifrados, cpf);
+        return decifragem_vernam(msg_codificada, cpf);
+    }
+
+    private String decifragem_vernam(String cifrado, String cpf)
+    {
+        return cifra_vernam(cifrado, cpf);
+    }
+
+    private String decifragem_AES(byte[] bytes_cifrados, String cpf)
+    {
+        try{
+            // Resgata a chave
+            SecretKey chave = servidor.getChaveAES(cpf);
+            
+            // Configura o decifrador
+            Cipher decifrador = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            decifrador.init(Cipher.DECRYPT_MODE, chave, servidor.getVetorInit(cpf));
+
+            // Decifra o array de bytes
+            byte [] bytes_decifrados = decifrador.doFinal(bytes_cifrados);
+
+            // Retorna a mensagem decifrada
+            return new String(bytes_decifrados);
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (NoSuchPaddingException e) {
+            e.printStackTrace();
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        } catch (InvalidAlgorithmParameterException e) {
+            e.printStackTrace();
+        } catch (IllegalBlockSizeException e) {
+            e.printStackTrace();
+        } catch (BadPaddingException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    /* ======================================= */
+    /*    GERACAO DE VETOR DE INICIALIZACAO    */
+    /* ======================================= */ 
+
+    public static IvParameterSpec gerar_vetor_inicializacao()
+    {
+        byte [] vi = new byte[16];
+        new SecureRandom().nextBytes(vi);
+        return new IvParameterSpec(vi);
     }
 }
---
+++ Servidor.java
@@ -0,0 +1,63 @@
+import java.util.Map;
+import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+
+/*
+ Classe responsavel por controlar a geracao e distribuicao 
+ de chaves, bem como realizar autenticacao nas mensagens 
+ trocadas com base nas chaves. 
+*/
+public class Servidor {
+    /* Chave = CPF do usuário, Value = chave AES */
+    private Map<String, SecretKey> chavesAES = null;
+    /* Chave = CPF do usuário, Value = chave Vernam */
+    private Map<String, String> chavesVernam = null;
+    /* Chave = CPF do usuário, Value = vetor de inicializacao (temporario) */
+    private Map<String, IvParameterSpec> vetores_init = null;
+
+    public Servidor()
+    {
+        chavesAES = new HashMap<String, SecretKey>();
+        chavesVernam = new HashMap<String, String>();
+        vetores_init = new HashMap<String, IvParameterSpec>();
+    }
+
+    /* ======================================= */
+    /*           GETTERS E SETTERS             */
+    /* ======================================= */ 
+
+    public SecretKey getChaveAES(String cpf) { return chavesAES.get(cpf); }
+    public String getChaveVernam(String cpf) { return chavesVernam.get(cpf); }
+    public IvParameterSpec getVetorInit(String cpf) { return vetores_init.get(cpf); }
+    
+    public void setVetorInit(String cpf, IvParameterSpec newVI) 
+    {
+        if(cpf.isBlank()){
+            System.out.println("CPF inválido!");
+            return;
+        }
+        if(newVI == null){
+            System.out.println("Vetor de Inicialização está nulo!");
+            return;
+        }
+        vetores_init.replace(cpf, newVI);
+    }
+
+    /* ======================================= */
+    /*      ADICAO DE UM CPF NO SERVIDOR       */
+    /* ======================================= */ 
+
+    public void addCPF(String cpf)
+    {
+        try {
+            chavesAES.put(cpf, Cifrador.gerarChaveAES());
+            chavesVernam.put(cpf, Cifrador.gerarChaveVernam());
+            vetores_init.put(cpf, Cifrador.gerar_vetor_inicializacao());
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        
+    }
+}
